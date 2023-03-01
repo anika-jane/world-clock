c6ce1ec37c8dabe4372780a19cc75abe8689c961
anika-jane <112495457+anika-jane@users.noreply.github.com>
2023-03-01 15:00:37 -0500

    updated Select
    Added city id to select
    
    JS - every time someone changes a select city, it is going to clean the nsame and time of the city and update the page with this information

--- SRC/index.js
+++ SRC/index.js
@@ -3,36 +3,63 @@
 function showCurrentTime(response) {
 
     let losAngelesElement = document.querySelector("#los-angeles");
+    if (losAngelesElement) {
 let losAngelesDateElement = losAngelesElement.querySelector(".date");
 let losAngelesTimeElement = losAngelesElement.querySelector(".time");
 let losAngelesTime = moment().tz("America/Los_Angeles");
 losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do, YYYY");
 losAngelesTimeElement.innerHTML = losAngelesTime.format("h:mm:ss [<small>]A[</small>]");
+}
 
 
 let newYorkElement = document.querySelector("#new-york");
+if (newYorkElement) {
 let newYorkDateElement = newYorkElement.querySelector(".date");
 let newYorkTimeElement = newYorkElement.querySelector(".time");
 let newYorkTime = moment().tz("America/New_York");
 newYorkDateElement.innerHTML = newYorkTime.format("MMMM Do, YYYY");
 newYorkTimeElement.innerHTML = newYorkTime.format("h:mm:ss [<small>]A[</small>]");
+}
 
 
 let londonElement = document.querySelector("#london");
+if (londonElement) {
 let londonDateElement = londonElement.querySelector(".date");
 let londonTimeElement = londonElement.querySelector(".time");
 let londonTime = moment().tz("Europe/London");
 londonDateElement.innerHTML = londonTime.format("MMMM Do, YYYY");
 londonTimeElement.innerHTML = londonTime.format("h:mm:ss [<small>]A[</small>]");
+}
 
 let tokyoElement = document.querySelector("#tokyo");
+if (tokyoElement) {
 let tokyoDateElement = tokyoElement.querySelector(".date");
 let tokyoTimeElement = tokyoElement.querySelector(".time");
 let tokyoTime = moment().tz("Asia/Tokyo");
 tokyoDateElement.innerHTML = tokyoTime.format("MMMM Do, YYYY");
 tokyoTimeElement.innerHTML = tokyoTime.format("h:mm:ss [<small>]A[</small>]");
-
 }
+}
+
+function updateCity(event) {
+    let cityTimeZone = event.target.value;
+    let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+    let cityTime = moment().tz(cityTimeZone);
+    let citiesElement = document.querySelector("#cities");
+    citiesElement.innerHTML = `
+    <div class="city">
+    <div>
+    <h2>${cityName}</h2>
+    <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+    </div>
+    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small></div>
+    </div>
+    `
+}
+
 
 showCurrentTime();
 setInterval(showCurrentTime, 100);
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
